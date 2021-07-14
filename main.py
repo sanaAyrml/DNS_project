145245b6ccd558817a77e4fb0fffad71bcfcdb0d
sana <ayromlous@gmail.com>
2021-07-14 23:50:10 +0430

    Add CA

---
+++ main.py
@@ -0,0 +1,43 @@
+from user import User
+from CA import CA
+from merchant import Merchant
+from bank import Bank
+from blockChain import BlockChain
+
+def ask_for_certificate(u1,u2,ca):
+    print(u2.uid)
+    csr, signed_csr, encrypted = u1.creat_csr(u2.uid)
+    certificate, signed_certificate,encrypted = ca.get_csr(csr, signed_csr, encrypted)
+    u1.get_certifcate(certificate, signed_certificate,encrypted)
+    return
+
+def deligation(u,bl,ca):
+    ask_for_certificate(u,bl,ca)
+    return
+
+ca = CA("CA", None)
+
+u = User(1,ca.pub_key)
+ca.add_pubkey(1,u.get_pubkey())
+
+
+m = Merchant(2,ca.pub_key)
+ca.add_pubkey(2,m.get_pubkey())
+
+
+bl = BlockChain(3,ca.pub_key)
+ca.add_pubkey(3,bl.get_pubkey())
+
+ba = Bank(4,ca.pub_key)
+ca.add_pubkey(4,ba.get_pubkey())
+
+deligation(u,bl,ca)
+
+
+# cipher_key,cipher_iv = u1.symmetric_key_with_CA()
+# ans = CA.set_symmetric_key(cipher_key,cipher_iv)
+# if ans:
+#     cipher_csr, cipher_signed_csr =u1.send_csr_pack_to_CA()
+# CA.get_csr_from_user(cipher_csr, cipher_signed_csr)
+# CA.verify_csr(signed_csr,csr)
+# u1.encrypt_csr()
